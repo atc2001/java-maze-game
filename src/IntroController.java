a01d4dd477036e021fa6f06e23f2434425630b00
Aaron Clark <atc2001@virginmedia.com>
2020-01-08 19:53:50 +0000

    remove import statement

--- src/IntroController.java
+++ src/IntroController.java
@@ -3,7 +3,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.TextField;
-import javafx.stage.Stage;
 
 import java.io.IOException;
 
